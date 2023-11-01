1bab412bb5b3d2396cd0a6e2e07fbbe477e8cf95
SamM71 <sammocock@gmail.com>
2023-11-01 18:02:35 +0000

    create remaining planets (all same size)

--- planets.js
+++ planets.js
@@ -17,6 +17,22 @@ const moon = new THREE.Mesh(
 // moon.position.setX(-7)
 // moon.position.setY(2)
 
+const mercuryTexture = new THREE.TextureLoader().load('mercury.jpg')
+export const mercury = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: mercuryTexture
+  })
+)
+
+const venusTexture = new THREE.TextureLoader().load('venus_atmosphere.jpg')
+export const venus = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: venusTexture
+  })
+)
+
 const earthTexture = new THREE.TextureLoader().load('earth.jpg')
 export const earth = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
@@ -30,5 +46,37 @@ export const mars = new THREE.Mesh(
   new THREE.SphereGeometry(1.5, 32, 32),
   new THREE.MeshStandardMaterial({
     map: marsTexture
+  })
+)
+
+const jupiterTexture = new THREE.TextureLoader().load('jupiter.jpg')
+export const jupiter = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: jupiterTexture
+  })
+)
+
+const saturnTexture = new THREE.TextureLoader().load('saturn.jpg')
+export const saturn = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: saturnTexture
+  })
+)
+
+const uranusTexture = new THREE.TextureLoader().load('uranus.jpg')
+export const uranus = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: uranusTexture
+  })
+)
+
+const neptuneTexture = new THREE.TextureLoader().load('neptune.jpg')
+export const neptune = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: neptuneTexture
   })
 )
